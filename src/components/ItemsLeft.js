8c4f339cb0111798c0580fc8474a3e7e10743ec0
jomefavourite <jfjomefavourite@gmail.com>
2020-12-08 00:27:28 +0100

    code
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- src/components/ItemsLeft.js
+++ src/components/ItemsLeft.js
@@ -3,7 +3,7 @@ import React from "react";
 //added logic to filter completed. U can extract it as a variable so that it's not as ugly 😁
 
 function ItemsLeft({todos}) {
-  return <>{todos.length - todos.filter(t => t.completed).length} items left</>;
+  return <>{todos.length - todos.filter(todo => todo.completed).length} items left</>;
 }
 
 export default ItemsLeft;
