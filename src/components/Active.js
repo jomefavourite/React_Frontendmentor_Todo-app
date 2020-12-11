692ff2d21a554d60a4117adf742d0b0a46d3ca23
codewithfahad <codewithfahad@gmail.com>
2020-12-11 17:41:34 +0500

    Fix Active and Completed components 🚀
    Correctly filter todos depending upon completed or active
    Fix empty div showing when no content

--- src/components/Active.js
+++ src/components/Active.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-const Active = ({sunVisibility, todos}) => {
+const Active = ({ sunVisibility, todos }) => {
   let todo = todos.length ? (
-    todos.map(todo => {
+    todos.map((todo) => {
       return (
         <div
           key={todo.id}
           className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx ${
             sunVisibility ? "bg-color10" : "bg-color3"
           } ${!todo.completed && "text-color11"}`}
+          style={{ display: todo.completed === false ? " " : "none" }}
         >
           {todo.completed === false ? todo.content : null}
         </div>
@@ -23,6 +24,30 @@ const Active = ({sunVisibility, todos}) => {
       No Todo Added
     </div>
   );
+
+  // checks if at least one todo is completed
+  const checkIfNoneActive = () => {
+    let showMsg = false;
+    todos.forEach((todo) => {
+      if (!todo.completed) {
+        showMsg = true;
+      }
+    });
+    return showMsg;
+  };
+
+  // if no todos are completed show msg
+  if (!checkIfNoneActive()) {
+    return (
+      <div
+        className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx text-color13 ${
+          sunVisibility ? "bg-color10" : "bg-color3"
+        }`}
+      >
+        None Active
+      </div>
+    );
+  }
 
   return <div>{todo}</div>;
 };
--- src/components/Completed.js
+++ src/components/Completed.js
@@ -1,36 +1,55 @@
-import React from "react";
+import { React } from "react";
 
-const Completed = ({sunVisibility, todos}) => {
-  let todo = todos.length ? (
-    todos.map(todo => {
-      return todo.completed ? (
-        <div
-          key={todo.id}
-          className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx text-color13 ${
-            sunVisibility ? "bg-color10" : "bg-color3"
-          } ${!todo.completed && "text-color11"}`}
-        >
-          {todo.completed && todo.content}
-        </div>
-      ) : (
-        <div
-          className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx text-color13 ${
-            sunVisibility ? "bg-color10" : "bg-color3"
-          }`}
-        >
-          None Completed
-        </div>
-      );
-    })
-  ) : (
-    <div
-      className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx text-color13 ${
-        sunVisibility ? "bg-color10" : "bg-color3"
-      }`}
-    >
-      No Todo Added
-    </div>
-  );
+const Completed = ({ sunVisibility, todos }) => {
+  if (todos.length === 0) {
+    return (
+      <div
+        className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx text-color13 ${
+          sunVisibility ? "bg-color10" : "bg-color3"
+        }`}
+      >
+        No Todo Added
+      </div>
+    );
+  }
+
+  let todo = todos.map((todo) => {
+    return (
+      <div
+        key={todo.id}
+        className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx text-color13 ${
+          sunVisibility ? "bg-color10" : "bg-color3"
+        } ${!todo.completed && "text-color11"}`}
+        style={{ display: todo.completed ? " " : "none" }}
+      >
+        {todo.content}
+      </div>
+    );
+  });
+
+  // checks if at least one todo is completed
+  const checkIfNoneCompleted = () => {
+    let showMsg = false;
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        showMsg = true;
+      }
+    });
+    return showMsg;
+  };
+
+  // if no todos are completed show msg
+  if (!checkIfNoneCompleted()) {
+    return (
+      <div
+        className={`mt-5 md:mt-0 text-center space-x-4 rounded-md md:rounded-t-none Todo flx text-color13 ${
+          sunVisibility ? "bg-color10" : "bg-color3"
+        }`}
+      >
+        None Completed
+      </div>
+    );
+  }
 
   return <div>{todo}</div>;
 };
